81d961a369a59503c6f4c0bb854acf2546166f6d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-10-28 23:37:20 -0500

    Add Context Global - Open/Close Sidebar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,12 +1,20 @@
+import { useContext } from 'react';
+
 import { Box, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
 import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
 
+import { UIContext } from '../../context/ui';
+
+
 const menuItems: string[] = ['Inbox', 'Starred', 'Send Email', 'Drafts'];
 
 export const Sidebar = () => {
+
+    const { sideMenuOpen, closeSideMenu } = useContext(UIContext);
+
     return (
-        <Drawer anchor="left" open={true} onClose={() => console.log("Cerrando")}>
+        <Drawer anchor="left" open={sideMenuOpen} onClose={closeSideMenu}>
             <Box sx={{ width: 250 }}>
                 <Box sx={{ padding: '5px 10px' }}>
                     <Typography variant="h4">Menú</Typography>
