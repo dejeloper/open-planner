7b9a76ee3b7887a3740cc734a07846992b9eb590
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-12-19 17:29:35 -0500

    Update Seed-data Endpoint

--- database/seed-data.ts
+++ database/seed-data.ts
@@ -12,21 +12,19 @@ interface SeedEntry {
 export const seedData: SeedData = {
   entries: [
     {
-      description: "Pendiente: Qui mollit ex anim sint laborum.",
-      status: "pending",
+      description: "Primera Tarea de Ejemplo.",
+      status: "finished",
       createAt: Date.now(),
     },
     {
-      description:
-        "En Progreso: Ut amet exercitation anim velit laboris pariatur deserunt labore laboris in.",
+      description: "Segunda Tarea de Ejemplo.",
       status: "in-progress",
       createAt: Date.now() + 100000,
     },
     {
-      description:
-        "Finalizada: Nostrud et dolor dolor dolore laboris aliquip sit consequat consequat.",
-      status: "finished",
+      description: "Tercera Tarea de Ejemplo.",
+      status: "pending",
       createAt: Date.now() + 300000,
-    },
+    }
   ]
 }
