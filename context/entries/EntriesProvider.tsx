55279e5d9f56b4a245833b97ee6cecd4abc4b284
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-12-20 17:57:47 -0500

    Update Save Entry from Front-end

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,4 @@
 import { FC, useEffect, useReducer } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 import { Entry } from "../../interfaces";
 import { entriesApi } from "../../apis";
@@ -20,17 +19,16 @@ interface Props {
 export const EntriesProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      status: "pending",
-      createAt: Date.now()
+  const addNewEntry = async (description: string) => {
+    try {
+      const { data } = await entriesApi.post<Entry>('/entries', { description });
+      dispatch({ type: '[Entry] - Add-Entry', payload: data })
+
+    } catch (error) {
+      console.log(error)
     }
-
-    dispatch({ type: '[Entry] - Add-Entry', payload: newEntry })
-
   }
+
 
   const updateEntry = (entry: Entry) => {
     dispatch({ type: '[Entry] - Updated-Entry', payload: entry });
