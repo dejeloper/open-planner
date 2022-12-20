b4534c9fdf8389b903e99ed1f2fecef012e47ad9
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-12-20 12:42:36 -0500

    Add Save Entry Endpoint

--- pages/api/v1/entries/index.ts
+++ pages/api/v1/entries/index.ts
@@ -13,6 +13,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     case 'GET':
       return getEntries(res)
 
+    case 'POST':
+      return addEntry(req, res)
+
     default:
       return res.status(400).json({ message: 'Endpoind no existe' })
   }
@@ -24,4 +27,28 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   return res.status(200).json(entries);
+}
+
+const addEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+  const { description = '', } = req.body;
+  const newEntry = new Entry({
+    description,
+    createAt: Date.now()
+  })
+
+  try {
+    await db.connect();
+    await newEntry.save();
+    await db.disconnect();
+
+    return res.status(201).json(newEntry)
+  }
+  catch (error) {
+    await db.disconnect();
+    console.log(error);
+
+    return res.status(500).json({ message: 'Error al ingresar Tarea' })
+  }
+
 }
