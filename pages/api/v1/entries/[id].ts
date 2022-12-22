d05c821cfb87c8994724a27f9ad5f998b54170ff
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-12-21 19:12:43 -0500

    Add try catch in Update Entry Endpoint

--- pages/api/v1/entries/[id].ts
+++ pages/api/v1/entries/[id].ts
@@ -44,13 +44,13 @@ const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 }
 
 const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-
   const { id } = req.query;
   await db.connect();
 
   const entryToUpdate = await Entry.findById(id);
 
   if (!entryToUpdate) {
+    await db.disconnect();
     return res.status(400).json({ message: 'No hay Tarea con ese Id: ' + id })
   }
 
@@ -59,9 +59,16 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     status = entryToUpdate.status
   } = req.body;
 
-  const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true })
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true })
 
-  await db.disconnect();
-  return res.status(200).json(updatedEntry!)
+    await db.disconnect();
+    return res.status(200).json(updatedEntry!)
+
+  } catch (error: any) {
+    await db.disconnect();
+    console.log(error);
+    return res.status(400).json({ message: "Error al actualizar Tarea." })
+  }
 
 }
